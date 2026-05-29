3dd8807c00a2d7a64295ec71617132283834fd73
Lars Brix <git@laxis.me>
2026-05-29 16:30:01 +0200

    feat(init): alert if init script fails

--- src/init.ts
+++ src/init.ts
@@ -3,14 +3,17 @@ import type { NS } from '@ns';
 const AUTO_LAUNCH = [
 	'infra/autopwn.js',
 	'infra/servers.js',
-	'feat/hack/orchestrator.js',
 	'feat/metrics.js',
+	'feat/hack/orchestrator.js',
 	// 'feat/hacknet.js',
 	//  'feat/stocks.js',
 ];
 
 export async function main(ns: NS) {
 	for (const script of AUTO_LAUNCH) {
-		ns.exec(script, ns.getHostname(), 1);
+		const pid = ns.exec(script, ns.getHostname(), 1);
+		if (!pid) {
+			ns.alert(`ERROR failed to launch ${script}`);
+		}
 	}
 }
