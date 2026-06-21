f00ffad21983b199697a94923872c1faca9109c4
Lars Brix <git@laxis.me>
2026-06-21 16:58:29 +0200

    feat(cloud): batch buys

--- src/feat/cloud.ts
+++ src/feat/cloud.ts
@@ -12,49 +12,44 @@ export async function main(ns: NS) {
 	ensureSingleton(ns);
 	const max = ns.cloud.getServerLimit();
 	while (true) {
-		const player = ns.getPlayer();
-		const maxCost = player.money * 0.1;
+		const purchases: string[] = [];
 
-		const servers = ns.cloud
-			.getServerNames()
-			.map((server) => ({ hostname: server, ram: ns.getServerMaxRam(server) }))
-			.sort((a, b) => a.ram - b.ram);
+		while (true) {
+			const maxCost = ns.getPlayer().money * 0.1;
+			const servers = ns.cloud
+				.getServerNames()
+				.map((server) => ({ hostname: server, ram: ns.getServerMaxRam(server) }))
+				.sort((a, b) => a.ram - b.ram);
 
-		// get max pow
-		let pow = 1;
-		do {
-			pow += 1;
-		} while (ns.cloud.getServerCost(2 ** pow) < maxCost);
-		const targetRam = 2 ** (pow - 1);
+			let pow = 1;
+			do {
+				pow += 1;
+			} while (ns.cloud.getServerCost(2 ** pow) < maxCost);
+			const targetRam = 2 ** (pow - 1);
 
-		if (targetRam < 8 || (servers.length >= max && targetRam <= servers[0].ram)) {
-			// not an increase
-			await ns.sleep(60_000);
-			continue;
+			if (targetRam < 8 || (servers.length >= max && targetRam <= servers[0].ram)) {
+				break;
+			}
+
+			const deletionCandidate = servers[0];
+			if (servers.length >= max && deletionCandidate) {
+				ns.writePort(Ports.Servers, { added: true, host: deletionCandidate.hostname });
+				ns.killall(deletionCandidate.hostname);
+				ns.cloud.deleteServer(deletionCandidate.hostname);
+				ns.writePort(Ports.Servers, { added: false, host: deletionCandidate.hostname });
+			}
+
+			const newServer = ns.cloud.purchaseServer(prefix, targetRam);
+			if (!newServer) break;
+			purchases.push(ns.format.ram(targetRam));
 		}
 
-		const deletionCandidate = servers[0];
-		let deletedSmallest = false;
-		if (servers.length >= max && deletionCandidate) {
-			ns.writePort(Ports.Servers, { added: true, host: deletionCandidate.hostname });
-			ns.killall(deletionCandidate.hostname);
-			ns.cloud.deleteServer(deletionCandidate.hostname);
-			ns.writePort(Ports.Metrics, {
-				type: 'log',
-				message: `-SERVER ${deletionCandidate.hostname}`,
-			} satisfies LogEvent);
-			deletedSmallest = true;
-			ns.writePort(Ports.Servers, { added: false, host: deletionCandidate.hostname });
-		}
-
-		const newServer = ns.cloud.purchaseServer(prefix, targetRam);
-		if (newServer) {
-			const message = `+SERVER ${newServer} (${ns.format.ram(targetRam)})`;
-			ns.writePort(Ports.Metrics, {
-				type: 'log',
-				message,
-			} satisfies LogEvent);
+		if (purchases.length > 0) {
+			const message = `+CLOUD ${purchases.join(', ')}`;
+			ns.writePort(Ports.Metrics, { type: 'log', message } satisfies LogEvent);
 			ns.print(`INFO ${message}`);
 		}
+
+		await ns.sleep(60_000);
 	}
 }
